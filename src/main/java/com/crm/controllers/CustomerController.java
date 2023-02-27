b4a74d0d6e32f3cb34a9ac387bebbbd763c7e500
AreejQadomi <areej.qadomi94@gmail.com>
2023-02-28 01:18:48 +0300

    Adding the customer service layer

--- src/main/java/com/crm/controllers/CustomerController.java
+++ src/main/java/com/crm/controllers/CustomerController.java
@@ -1,10 +1,11 @@
 package com.crm.controllers;
 
-import com.crm.dao.CustomerDAO;
 import com.crm.models.Customer;
+import com.crm.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,14 +14,14 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    //Inject the CustomerDAO
+    //Inject the Customer service - so we don't talk to the DAO directly
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model model) {
         // get the customers from the DAO
-        List<Customer> customerList = customerDAO.getCustomers();
+        List<Customer> customerList = customerService.getCustomers();
 
         // Add the customers to the model object
         model.addAttribute("customerList", customerList);
---
+++ src/main/java/com/crm/services/CustomerService.java
@@ -0,0 +1,10 @@
+package com.crm.services;
+
+import com.crm.models.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    List<Customer> getCustomers();
+}
